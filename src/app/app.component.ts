dfcd82ea954ccf4e2cfadab69c3eaaec55d4abeb
omer <omerg206@gmail.com>
2021-03-08 18:17:42 +0200

    table zoom and add data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Chart, ChartType } from 'chart.js';
 import 'chartjs-plugin-zoom';
-import 'hammerjs';
+import { element } from 'protractor';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,91 +10,54 @@ import 'hammerjs';
 })
 export class AppComponent implements AfterViewInit {
   @ViewChild('graph', { static: true, read: ElementRef }) graph!: ElementRef;
-  ctx:  CanvasRenderingContext2D | null = null;
+  ctx: CanvasRenderingContext2D | null = null;
   myChart: Chart | null = null;
-  config : Chart.ChartConfiguration | null = null;
+  config: Chart.ChartConfiguration | null = null;
 
   ngAfterViewInit(): void {
-    this.ctx =  this.graph.nativeElement.getContext('2d');
+    this.ctx = this.graph.nativeElement.getContext('2d');
     this.config = {
       type: 'bar',
       data: {
         datasets: [
           {
-          label: "Dataset 1",
-          data: [{
-            x: new Date(Date.now()),
-            y: Math.floor(100 *Math.random())
-          }, {
-            x: new Date(Date.now() - 1 *1000),
-            y: Math.floor(100 *Math.random())
-          }, {
-            x: new Date(Date.now() - 2 *1000),
-            y: Math.floor(100 *Math.random())
-          }, {
-            x: new Date(Date.now() - 3 *1000),
-            y: Math.floor(100 *Math.random())
-          }],
-          backgroundColor: "rgba(54, 162, 235,0.9)",
-          hoverBackgroundColor: "rgba(54, 162, 235, 1)",
-          borderColor: "rgba(54, 162, 235,0.9)",
-          hoverBorderWidth: 2,
-          hoverBorderColor: "yellow",
-          borderJoinStyle: 'round',
-          fill: false
-        },
-        {
-          label: "Dataset 2",
-          data: [{
-            x: new Date(Date.now()),
-            y: Math.floor(100 *Math.random())
-          }, {
-            x: new Date(Date.now() - 1 *1000),
-            y: Math.floor(100 *Math.random())
-          }, {
-            x: new Date(Date.now() - 2 *1000),
-            y: Math.floor(100 *Math.random())
-          }, {
-            x: new Date(Date.now() - 3 *1000),
-            y: Math.floor(100 *Math.random())
-          }],
-          backgroundColor: "rgba(75, 192, 192, 0.8)",
-          hoverBackgroundColor: "rgba(75, 192, 192, 1)",
-          borderColor: "rgba(75, 192, 192, 0.8)",
-          hoverBorderWidth: 2,
-          hoverBorderColor: "red",
-          fill: false
-        },
-        {
-          label: "Dataset 3",
-          data: [{
-            x: new Date(Date.now()),
-            y: Math.floor(100 *Math.random())
-          }, {
-            x: new Date(Date.now() - 1 *1000),
-            y: Math.floor(100 *Math.random())
-          }, {
-            x: new Date(Date.now() - 2 *1000),
-            y: Math.floor(100 *Math.random())
-          }, {
-            x: new Date(Date.now() - 3 *1000),
-            y: Math.floor(100 *Math.random())
-          }],
-          backgroundColor: "rgba(204, 102, 153, 0.8)",
-          hoverBackgroundColor: "rgba(204, 102, 153, 1)",
-          borderColor: "rgba(204, 102, 153, 0.8)",
-          hoverBorderWidth: 2,
-          hoverBorderColor: "brown",
-          fill: false
-        }
-      ]
+            label: "Dataset 1",
+            backgroundColor: "rgba(54, 162, 235,0.9)",
+            hoverBackgroundColor: "rgba(54, 162, 235, 1)",
+            borderColor: "rgba(54, 162, 235,0.9)",
+            hoverBorderWidth: 2,
+            hoverBorderColor: "yellow",
+            borderJoinStyle: 'round',
+          },
+          {
+            label: "Dataset 2",
+            backgroundColor: "rgba(75, 192, 192, 0.8)",
+            hoverBackgroundColor: "rgba(75, 192, 192, 1)",
+            borderColor: "rgba(75, 192, 192, 0.8)",
+            hoverBorderWidth: 2,
+            hoverBorderColor: "red",
+          },
+          {
+            label: "Dataset 3",
+            backgroundColor: "rgba(204, 102, 153, 0.8)",
+            hoverBackgroundColor: "rgba(204, 102, 153, 1)",
+            borderColor: "rgba(204, 102, 153, 0.8)",
+            hoverBorderWidth: 2,
+            hoverBorderColor: "brown",
+          }
+        ]
       },
       options: {
+        elements: {
+          line: {
+            fill: false
+          }
+        },
         responsive: true,
-       title: {
-         display: true,
-         text: "my chart"
-       },
+        title: {
+          display: true,
+          text: "my chart"
+        },
         tooltips: {
           mode: "index",
           intersect: false
@@ -101,33 +65,36 @@ export class AppComponent implements AfterViewInit {
         scales: {
 
           xAxes: [{
-             stacked: true,
-             display: true,
+            stacked: true,
+            display: true,
             offset: true,
+            // distribution: 'series',
             type: "time",
             time: {
               unit: 'second',
-              round: 'second',
+              // round: 'second',
+              stepSize: 5,
               displayFormats: {
-                second: "ss"
+                second: "HH:mm:ss"
               },
-              parser: "ss",
-              // scaleLabel: {
-              //   display: true,
-              //   labelString: "Date"
-              // },
+              // parser: "ss",
+              scaleLabel: {
+                display: true,
+                labelString: "Date"
+              }
+
             }
           }],
           yAxes: [{
-              stacked: true,
-          //    display: true,
-          //    scaleLabel: {
-          //     display:     true,
-          //     labelString: 'value'
-          // },
-          // ticks: {
-          //   // beginAtZero: true
-          // }
+            stacked: true,
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'value'
+            },
+            // ticks: {
+            //   // beginAtZero: true
+            // }
 
           }]
         },
@@ -140,17 +107,31 @@ export class AppComponent implements AfterViewInit {
 
               // Panning directions. Remove the appropriate direction to disable
               // Eg. 'y' would only allow panning in the y direction
-              mode: 'xy'
+              mode: 'xy',
+              // rangeMax: {
+              //   x: 4000
+              // },
+              // rangeMin: {
+              //   x: 0
+              // }
             },
 
             // Container for zoom options
             zoom: {
               // Boolean to enable zooming
               enabled: true,
+              // drag: true,
 
               // Zooming directions. Remove the appropriate direction to disable
               // Eg. 'y' would only allow zooming in the y direction
               mode: 'xy',
+              // rangeMax: {
+              //   x: 20000
+              // },
+              // rangeMin: {
+              //   x: 1000
+              // },
+              threshold: 10
             }
           }
         }
@@ -159,19 +140,47 @@ export class AppComponent implements AfterViewInit {
 
 
     this.myChart = new Chart(this.ctx, this.config);
-
+    this.randomizeData();
     // console.log(  (myChart).resetZoom);
 
   }
 
 
-  changeGraphType(graphType: ChartType){
+  changeGraphType(graphType: ChartType) {
     this.myChart.destroy();
-    this.myChart = new Chart(this.ctx, {...this.config, type: graphType});
+    this.myChart = new Chart(this.ctx, { ...this.config, type: graphType });
   }
 
   resetZoom() {
     (this.myChart as any).resetZoom();
   }
 
+
+  randomizeData() {
+    const labels = ["Dataset 1", "Dataset 2", "Dataset 3"];
+    const dataNum = 60;
+    labels.forEach((label: string, index: number) => {
+      const newData: Chart.ChartPoint[] = this.myChart.data.datasets[index].data as Chart.ChartPoint[];
+
+      for (let i = 0; i < dataNum; ++i) {
+        if (!this.myChart.data.datasets[index].data) {
+          this.myChart.data.datasets[index].data = []
+        }
+
+        (this.myChart.data.datasets[index].data as Chart.ChartPoint[]).push({
+          x: Date.now() - ((i + 60000) * 1000),
+          y: this.randomScalingFactor()
+        })
+
+      }
+
+    })
+
+    this.myChart.update();
+  }
+
+
+  randomScalingFactor() {
+    return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100);
+  }
 }
